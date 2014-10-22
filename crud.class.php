d2da45d843d1f9df58ae5c26beadc2dcf7ce4be3
Jonathan Narzo <jonathan.narzo@gmail.com>
2014-10-22 22:37:30 +0800

    pdo connection to database and create method

---
+++ crud.class.php
@@ -0,0 +1,30 @@
+<?php
+
+class Crud extends Database
+{
+
+	/**
+	 * Recieved values from the create form
+	 *
+	 * $name and $email
+	 *
+	 **/
+	public function create($name, $email)
+	{
+		$vals = array(null, $name, $email); // values to be passed and inserted, null auto incremented value in database
+		try
+		{
+			$q = $this->conn->prepare("INSERT INTO users VALUES (?,?,?)");
+			$q->execute($vals); //passed values on array
+			if($q)
+			{
+				header('Location: '.$_SERVER['PHP_SELF'].'');
+			}
+		}
+		catch (PDOException $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+}
